866757dc24e167f75a78b3aa1948c1dcdf43b158
Axel Flores <axelfloresteja@protonmail.com>
2025-09-19 09:01:12 -0600

    encryption aaaaaa

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from mod import generalPurpose, passwordBase
+
+from cryptography.fernet import Fernet
 import json
 import os
+
+key = b''
 
 filename = "pass.json"
 database = []
 
+f = None
+
 if os.path.isfile(filename) == False:
     passwordBase.create_database(filename)
+    key = Fernet.generate_key()
+    f = Fernet(key)
+    print(f"the key for the database, store it on a secure place: {key.decode("utf-8")}")
 else:
     with open(filename, "r") as read_file:
-        database = json.load(read_file)   
+        key = bytes(input("enter key to unlock database: "), "utf-8")
+        try:
+            f = Fernet(key)
+        except:
+            print("Invalid key")
+            exit(-1)
+        database = json.loads(f.decrypt(read_file.read()))
         database = generalPurpose.quicksort(database)
 
 
@@ -29,4 +44,5 @@ passwordBase.add_entry(passwordBase.create_entry(words[0], words[1]), database)
 database = generalPurpose.quicksort(database)
 
 with open(filename, "w") as write_file:
-    json.dump(database, write_file, indent=4)
+    encrypted_file = f.encrypt(bytes(json.dumps(database, indent=4), "utf-8"))
+    write_file.write(encrypted_file.decode("utf-8"))
